2a0b8e064c04891381018aeffda7b1b097a1b139
yumi <yumkoori@gmail.com>
2024-11-11 19:53:16 +0900

    Feat: 편의점 기능을 위한 Application클래스 작성

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,28 @@
 package store;
 
+import java.util.Map;
+
 public class Application {
+    private static final Convenience convenience = new ConvenienceImpl();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            while (true) {
+                convenience.loadProduct();
+
+                String inputItem = InputView.readItem();
+                Map<String, Integer> purchaseItems = convenience.buy(inputItem);
+
+                //영수증 뽑기
+                convenience.getReceipt(purchaseItems);
+
+                String answer = InputView.inputReStart();
+                if (answer.equals("N")) {
+                    break;
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
